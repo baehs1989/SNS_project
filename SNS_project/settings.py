4200a69094f3294a7983fe18b093a9f70dc3cafe
HyungSoo Bae <hyungsoobae@HyungSooui-MacBook-Pro.local>
2018-04-07 23:39:53 -0400

    setting.py updated

--- SNS_project/settings.py
+++ SNS_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*$_0^$kq86n7$_=@tejv*rn+d10@-=lyvtr+ll5zur$hs$lh)y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuaap.com']
+ALLOWED_HOSTS = ['.herokuapp.com','sns-project.herokuapp.com']
 
 
 # Application definition
