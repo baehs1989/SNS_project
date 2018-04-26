42f3964f9ad0e4156217446836affbc8e624b4ee
HyungSoo Bae <hyungsoobae@HyungSooui-MacBook-Pro.local>
2018-04-25 23:11:24 -0400

    cookie function added

--- SNS_project/views.py
+++ SNS_project/views.py
@@ -2,3 +2,15 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super(HomePage, self).render_to_response(context, **response_kwargs)
+        response.set_cookie("visited","visited",max_age=30)
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cookie = self.request.COOKIES.get("visited")
+        if cookie:
+            context['visited'] = cookie
+        return context
